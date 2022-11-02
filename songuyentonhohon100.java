bb816b052c7adfc8f97964e1cf0cfc5876ea175e
tudao888 <daotu2106@gmail.com>
2022-11-02 10:56:34 +0700

    2

---
+++ songuyentonhohon100.java
@@ -0,0 +1,15 @@
+public class songuyentonhohon100 {
+    public static void main(String[] args) {
+        for (int i = 2; ; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i%j == 0) {
+                    count++;
+                }
+            }
+            if (count == 2 && i < 100) {
+                System.out.println(i + " , ");
+            }
+        }
+    }
+}
